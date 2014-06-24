ab0120bf3c7204f40937df487ad1faea48bbfb2c
Heiko Schocher <hs@denx.de>
2014-06-24 10:10:03 +0200

    lib, linux: move linux specific defines to linux/compat.h
    - move linux specific defines from usb and video code
      into linux/compat.h
    - move common linux specific defines from include/ubi_uboot.h
      to linux/compat.h
    - add for new mtd/ubi/ubifs sync new needed linux specific
      defines to linux/compat.h
    
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Cc: Marek Vasut <marex@denx.de>
    Cc: Anatolij Gustschin <agust@denx.de>
    [trini: Add spin_lock_irqsave/spin_unlock_irqrestore dummies from
    usb/lin_gadet_compat.h]
    Signed-off-by: Tom Rini <trini@ti.com>
    
    Conflicts:
    	drivers/usb/gadget/ether.c
    	drivers/usb/musb-new/linux-compat.h
    	drivers/video/exynos_dp.c
    	drivers/video/exynos_mipi_dsi.c
    	lib/Makefile

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -19,6 +19,7 @@
 #define __LINUX_USB_GADGET_H
 
 #include <errno.h>
+#include <linux/compat.h>
 #include <linux/list.h>
 
 struct usb_ep;
@@ -412,11 +413,6 @@ struct usb_gadget_ops {
 	int	(*udc_start)(struct usb_gadget *,
 			     struct usb_gadget_driver *);
 	int	(*udc_stop)(struct usb_gadget *);
-};
-
-struct device {
-	void		*driver_data;	/* data private to the driver */
-	void            *device_data;   /* data private to the device */
 };
 
 /**
