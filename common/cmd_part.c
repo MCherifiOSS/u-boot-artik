6f486136cb195e93c3fe928be262d36863d4b726
Paul Kocialkowski <contact@paulk.fr>
2015-06-15 21:35:04 +0200

    common: cmd_part: Proper alignment
    This fixes a misaligned declaration.
    
    Signed-off-by: Paul Kocialkowski <contact@paulk.fr>

--- common/cmd_part.c
+++ common/cmd_part.c
@@ -102,7 +102,7 @@ int do_part_list(int argc, char * const argv[])
 	if (var != NULL) {
 		int p;
 		char str[512] = { '\0', };
-	  disk_partition_t info;
+		disk_partition_t info;
 
 		for (p = 1; p < 128; p++) {
 			char t[5];
