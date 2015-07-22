e53d76728df1946bbdd2ad9c15fd981514a6a42d
Chanho Park <chanho61.park@samsung.com>
2015-07-22 20:50:25 +0900

    common: remove endif of CONFIG_PPC
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- include/common.h
+++ include/common.h
@@ -342,7 +342,6 @@ int	saveenv	     (void);
 int	setenv	     (const char *, const char *);
 int setenv_ulong(const char *varname, ulong value);
 int setenv_addr(const char *varname, const void *addr);
-#endif /* CONFIG_PPC */
 #ifdef CONFIG_ARM
 # include <asm/mach-types.h>
 # include <asm/setup.h>
